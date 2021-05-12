a42a6df917ca8c7479035bd13b266b2707fdde93
gp2112 <guipaixao2000@gmail.com>
2021-05-12 16:00:06 -0300

    add funcoes de insercao

--- binparse.h
+++ binparse.h
@@ -10,4 +10,11 @@ VEICULO *bin_get_veiculo(FILE *fp, int codLinha);
 
 LINHA *bin_get_linha(FILE *fp, int codLinha);
 
+
+// Indentificar campos nulos como "NULO" //
+void insert_veiculo(FILE *fp, VEICULO *v);
+
+void insert_linha(FILE *fp, LINHA *v);
+//////////////////////////////////////////
+
 #ifndef
