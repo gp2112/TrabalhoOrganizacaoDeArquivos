5577e05fc784d4c9fc5ef1ccdb5d1614e1cca101
gp2112 <guipaixao2000@gmail.com>
2021-05-13 19:07:33 -0300

    get_linha e readline

--- csvparse.c
+++ csvparse.c
@@ -1,13 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dados.h"
+
+#define BUFFER_SIZE 64
+/*
+typedef struct {
+
+	int codLinha;
+	char aceitaCartao;
+	char *nomeLinha;
+	char *corLinha;
+	char removido;
+	int tamanhoRegistro;
+	int tamanhoNome;
+	int tamanhoCor;
+
+} LINHA;
+
+*/
+
+char *readline(FILE *in) {
+	char *buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	char c; int i=0;
+
+	while ((c=fgetc(in)) == '\n' || c=='\r')
+		if (c == EOF) return NULL;
+
+
+	do {
+		if (i>0 && i%BUFFER_SIZE == 0)
+			buffer = (char *)realloc(buffer, BUFFER_SIZE * (int)(1 + BUFFER_SIZE/i)*sizeof(char));
+
+		buffer[i++] = c;
+
+	} while ((c=fgetc(in)) != '\n' && c!='\r' && c!=EOF);
+
+	buffer[i] = '\0';
+	return buffer;
+}
 
 
 LINHA *get_linha(FILE *fp) {
 	LINHA *linha = (LISTA *)malloc(sizeof(LISTA));
 
-	char *temp;
-	fscanf("%[,^\n]", temp);
+	char *line = readline(fp), *temp=NULL;
+	if (line == NULL) return NULL;
+
+	temp = strtok(line, ",");
+	linha->codLinha = atoi(temp);
+
+	temp = strtok(NULL, ",");
+	linha->aceitaCartao = temp[0];
+
+	temp = strtok(NULL, ",");
+	linha->nomeLinha = (char *)calloc(strlen(temp), sizeof(char));
+	linha->nomeLinha = strcpy(linha->nomeLinha, temp);
+
+	temp = strtok(NULL, ",");
+	linha->corLinha = (char *)calloc(strlen(temp), sizeof(char));
+	linha->corLinha = strcpy(linha->corLinha, temp);
+
+	linha->removido = '0';
+	linha->tamanhoNome = strlen(linha->nomeLinha);
+	linha->tamanhoCor = strlen(linha->corLinha);
+
+	/////////linha->tamanhoRegistro = ;//////
+
+	free(line);
 
 	return linha;
 }
