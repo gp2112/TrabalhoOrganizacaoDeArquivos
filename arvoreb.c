68992a6ea7c417edf555a50b5e943505c86797ad
gp2112 <guipaixao2000@gmail.com>
2021-07-10 21:36:05 -0300

    comentarios

--- arvoreb.c
+++ arvoreb.c
@@ -4,9 +4,22 @@
 #include "binparse.h"
 #include "binout.h"
 
+
+// IS_D irá executar o código para debug caso DEBUG = 1 //
 #define DEBUG 0
 #define IS_D if(DEBUG)
+//////////////////////////////////////////////////////////
 
+/*
+
+Implementação das funções relativas a árvore B
+
+INDEX_HEADER -> struct do header do arquivo de índices
+INDEX_REG -> struct da página do arquivo de índices (nó)
+
+*/
+
+//Cria o header do'arquivo de índice com valores iniciais
 INDEX_HEADER *header_index_create() {
 	INDEX_HEADER *index = (INDEX_HEADER *)malloc(sizeof(INDEX_HEADER));
 
@@ -19,6 +32,8 @@ INDEX_HEADER *header_index_create() {
 	return index;
 }
 
+// Cria uma nova página com valores padrões
+// Valores nulos tratados com -1
 INDEX_REG *create_indexreg(int rrn) {
 
 	INDEX_REG *index_reg = (INDEX_REG *)malloc(sizeof(INDEX_REG));
@@ -59,7 +74,7 @@ int binary_search(int *list, int key, int start, int end) {
 
 
 
-// imprime um nó
+// imprime um nó (para Debug)
 void print_node(INDEX_REG *node) {
 	if (node==NULL) {
 		printf("NO NULO!!!!!\n");
@@ -82,6 +97,7 @@ void print_node(INDEX_REG *node) {
 	printf("\n");
 
 }
+
 // retorna o byteoffset da chave ou retorna -1 caso não ache
 int64 btree_search(char *fname, int rrn, int key) {
 	if (rrn == -1)
@@ -90,7 +106,7 @@ int64 btree_search(char *fname, int rrn, int key) {
 	INDEX_REG *node = bin_get_index_reg(fname, rrn);
 
 	int i = binary_search(node->keys, key, 0, node->nroChavesIndexadas);
-	print_node(node);
+	IS_D print_node(node);
 
 	if (node->keys[i] == key) {
 		int64 r = node->pos[i];
@@ -103,7 +119,7 @@ int64 btree_search(char *fname, int rrn, int key) {
 	return btree_search(fname, next, key);
 }
 
-// percorre a arvore para imprimir os nos 
+// percorre a arvore para imprimir os nos (para Debug)
 void btree_print(char *fname, int rrn) {
 
 	INDEX_REG *node = bin_get_index_reg(fname, rrn);
@@ -122,13 +138,26 @@ void btree_print(char *fname, int rrn) {
 	free(node);
 }
 
-
+/* Funcão que realiza o algoritmo de Split da inserção na árvore
+   Parâmetros: 
+   	-fname -> Nome do arquivo de registro
+   	-node -> nó em que será feito o "split"
+   	-new_node -> nó a ser criado
+   	-promo_key -> chave promovida
+   	-promo_pos -> posicao da chave promovida
+   	-promo_child -> rrn do filho promovido  
+*/
 void split(char *fname, INDEX_REG *node, INDEX_REG *new_node, int *promo_key, int *promo_pos, int *promo_child) {
 	IS_D printf("\n======SPLIT!========\n");
-	int keys[ORDEM], pos[ORDEM], children[ORDEM+1];
-
 	IS_D printf("promokey antes: %d\n", *promo_key);
 
+
+	// cria arrays temporários para auxiliar o split 
+	int keys[ORDEM], pos[ORDEM], children[ORDEM+1];
+
+
+	// adiciona as chaves/posicoes/filhos do nó a ser dividido e do novo nó
+	// nos arrays auxiliares. Os remove dos nós originais (add -1).
 	for (int i=0; i<ORDEM; i++) {
 		if (i<ORDEM-1) {
 			keys[i] = node->keys[i];
@@ -145,7 +174,7 @@ void split(char *fname, INDEX_REG *node, INDEX_REG *new_node, int *promo_key, in
 	// acha a posicao da chave
 	int index = binary_search(keys, *promo_key, 0, ORDEM-1);
 	
-	// inserção
+	// remanejamento à direita para inserção ordenada
 	for (int i=ORDEM; i>index; i--) {
 		if (i<ORDEM) {
 			keys[i] = keys[i-1];
@@ -159,10 +188,9 @@ void split(char *fname, INDEX_REG *node, INDEX_REG *new_node, int *promo_key, in
 		printf("%d ", children[i]);
 	IS_D printf("\n");
 
-	// aqui
+	// insere a chave/posicao/filho na posicao correta
 	keys[index] = *promo_key;
 	pos[index] = *promo_pos;
-
 	children[index+1] = *promo_child;
 
 	IS_D printf("vetor filhos depois:\n");
@@ -170,17 +198,22 @@ void split(char *fname, INDEX_REG *node, INDEX_REG *new_node, int *promo_key, in
 		printf("%d ", children[i]);
 	IS_D printf("\n");
 
-	*promo_key = keys[(int)(ORDEM/2)]; // chave a esquerda da direita
-	*promo_pos = pos[(int)(ORDEM/2)];
+
+	// promove a chave/posicao do meio
+	*promo_key = keys[((int)(ORDEM/2))];
+	*promo_pos = pos[((int)(ORDEM/2))];
+
+	// promove rrn do novo nó
 	*promo_child = new_node->RRNdoNo;
 
-	for (int i=0; i<(int)(ORDEM/2); i++) {
+	// redistribui as chaves/posicoes/filhos, balanceados
+	for (int i=0; i<((int)(ORDEM/2)); i++) {
 		node->keys[i] = keys[i];
 		node->pos[i] = pos[i];
 		node->children[i] = children[i];
-		new_node->keys[i] = keys[(int)(ORDEM/2)+1+i];
-		new_node->pos[i] = pos[(int)(ORDEM/2)+1+i];
-		new_node->children[i] = children[(int)(ORDEM/2)+i+1];
+		new_node->keys[i] = keys[((int)(ORDEM/2))+1+i];
+		new_node->pos[i] = pos[((int)(ORDEM/2))+1+i];
+		new_node->children[i] = children[((int)(ORDEM/2))+i+1];
 	}
 	node->children[(int)(ORDEM/2)] = children[(int)(ORDEM/2)];
 	new_node->children[(int)(ORDEM/2)] = children[ORDEM];
@@ -188,44 +221,62 @@ void split(char *fname, INDEX_REG *node, INDEX_REG *new_node, int *promo_key, in
 	node->nroChavesIndexadas = (int)(ORDEM/2);
 	new_node->nroChavesIndexadas = (int)(ORDEM/2);
 
+	// verifica se o nó é folha
 	if (new_node->children[0]==-1)
-		new_node->folha = '1';
+		new_node->folha = '1';      
 }	
 
+/* Funcão para inserção na árvore B
+   Parâmetros: 
+   	-fname -> Nome do arquivo de registro
+   	-header -> header do arquivo de registro
+   	-rrn -> rrn do nó atual, em que há a inserção
+   	-key -> a chave a ser inserida
+   	-pos -> posição da chave no arquivo de registro
+   	-promo_child -> rrn do filho promovido  
+   	-promo_pos -> posicao da chave promovida
+   	-promo_key -> chave promovida
+*/
 int btree_insert(char *fname, INDEX_HEADER *header, int rrn, int key, int pos, int *promo_child, int *promo_pos, int *promo_key) {
 
+	//se chegou em um nó folha, promove a chave/posicao a ser inserida
 	if (rrn == -1) {
-		IS_D printf("mudei -1\n");
 		*promo_key = key;
 		*promo_pos = pos;
 		*promo_child = -1;
 		return PROMOTION;
 	}
 
-	// le a pagina
-	
 	IS_D printf("RRN %d\n", rrn);
+	
+	// le a pagina no rrn
 	INDEX_REG *node = bin_get_index_reg(fname, rrn);
 	
 	
 	IS_D print_node(node);
 
+	// procura a posicão correta para inserir a chave
 	int i = binary_search(node->keys, key, 0, node->nroChavesIndexadas);
 
+	// realiza a inserção no nó filho na posição encontrada pela busca binária
 	int r = btree_insert(fname, header, node->children[i], key, pos, promo_child, promo_pos, promo_key);
 	
 	IS_D printf("Entrada promokey: %d\n", *promo_child);
 
+	// caso não exista promoção, retorna
 	if (r == NO_PROMOTION) {
 		free(node);
-		return r;
+		return NO_PROMOTION;
 	}
 
+
+	// Caso encontre um nó pai com espaço, realiza a inserção neste nó
 	if (node->nroChavesIndexadas < ORDEM-1) {
 		IS_D printf("É o PAI\n");
 		
 		i = binary_search(node->keys, *promo_key, 0, node->nroChavesIndexadas);
 
+		// remaneja os arrays para a inserção ordenada
 		for (int j=ORDEM-1; j>i; j--) {
 			if (j<=node->nroChavesIndexadas) {
 				node->keys[j] = node->keys[j-1];
@@ -233,22 +284,30 @@ int btree_insert(char *fname, INDEX_HEADER *header, int rrn, int key, int pos, i
 			}
 			node->children[j] = node->children[j-1];
 		}
+		// insere a chave e posicao nas posicoes corretas
 		node->keys[i] = *promo_key;
 		node->pos[i] = *promo_pos;
+
 		node->nroChavesIndexadas++;
 		
+		// se ouver filho promovido, atribui à direita da chave promovida
+		// e o nó passa a não ser folha
 		if (*promo_child != -1) {
 			node->children[i+1] = *promo_child;
 			node->folha = '0';
 		}
 
 		IS_D print_node(node);
+
+		// escreve a página no arquivo de índice
 		escreve_index_data(fname, node);
 		free(node);
 		return NO_PROMOTION;
 	}
 
+	// caso não haja espaço no nó pai, realiza split
 
+	// cria o novo nó para o split
 	INDEX_REG *new_node = create_indexreg(header->RRNproxNo);
 
 	split(fname, node, new_node, promo_key, promo_pos, promo_child);
@@ -259,6 +318,7 @@ int btree_insert(char *fname, INDEX_HEADER *header, int rrn, int key, int pos, i
 	IS_D print_node(new_node); 
 	IS_D printf("\n");
 
+	// escreve os nós no arquivo de índice
 	escreve_index_data(fname, node);
 	escreve_index_data(fname, new_node);
 	header->RRNproxNo++;
--- operacoes.c
+++ operacoes.c
@@ -266,19 +266,27 @@ ERROR operation9(char *bin_fname, char *bin_index) {
 	if (bin_data==NULL || index_f == NULL)
 		return FILE_ERROR;
 
+	VEICULO_HEADER *header_veiculo = bin_get_header_veiculo(bin_data);
+
+	if (header_veiculo->status == '0') {
+		free(header_veiculo);
+		return FILE_ERROR;
+	}
+
 	// cria arquivo de índice e escreve header
 	INDEX_HEADER *index_header = header_index_create();
 	escreve_header_index(bin_index, index_header);
 
 	
-	VEICULO_HEADER *header_veiculo = bin_get_header_veiculo(bin_data);
 
 	VEICULO *veiculo = NULL; 
 
 	// cria raiz
 	INDEX_REG *root = create_indexreg(0);
 	root->folha = '1';
+
 	escreve_index_data(bin_index, root);
+
 	index_header->RRNproxNo++;
 	index_header->noRaiz = 0;
 	free(root); root=NULL;
@@ -299,17 +307,24 @@ ERROR operation9(char *bin_fname, char *bin_index) {
 
 			insert_return = btree_insert(bin_index, index_header, index_header->noRaiz, convertePrefixo(veiculo->prefixo), byteoffset, &promo_child, &promo_pos, &promo_key);
 
+			// verifica se houve promoção
+			// se houve, haverá a criação de uma nova raiz
 			if (insert_return == PROMOTION) {
-
+				// cria novo nó raiz
 				INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
+
+				// adiciona a chave/posicao e os filhos no novo nó
 				new_root->nroChavesIndexadas++;
 				new_root->keys[0] = promo_key;
 				new_root->pos[0] = promo_pos;
 				new_root->children[0] = index_header->noRaiz;
 				new_root->children[1] = promo_child;
+
+				// atualiza no header a nova raiz
 				index_header->noRaiz = new_root->RRNdoNo;
 				index_header->RRNproxNo++;
 				
+				// salva o nó em disco
 				escreve_index_data(bin_index, new_root);
 				
 				IS_D printf("NEW ROOT\n");
@@ -318,6 +333,8 @@ ERROR operation9(char *bin_fname, char *bin_index) {
 			}
 		}
 		veiculo_delete(&veiculo);
+
+		// salva a posição do próximo veículo
 		byteoffset = ftell(bin_data);
 	}
 
@@ -343,15 +360,19 @@ ERROR operation10(char *bin_fname, char *bin_index) {
 			*index_f = fopen(bin_index, "wb"); //cria arquivo de indice
 	fclose(index_f);
 
-	if (bin_data==NULL || index_f == NULL)
+	if (bin_data==NULL)
 		return FILE_ERROR;
+	LINHA_HEADER *header_linha = bin_get_header_linha(bin_data);
+	if (header_linha->status == '0') {
+		free(header_linha);
+		return FILE_ERROR;
+	}
 
 	// cria arquivo de índice e escreve header
 	INDEX_HEADER *index_header = header_index_create();
 	escreve_header_index(bin_index, index_header);
 
 	
-	LINHA_HEADER *header_linha = bin_get_header_linha(bin_data);
 
 	LINHA *linha = NULL; 
 
@@ -379,17 +400,24 @@ ERROR operation10(char *bin_fname, char *bin_index) {
 
 			insert_return = btree_insert(bin_index, index_header, index_header->noRaiz, linha->codLinha, byteoffset, &promo_child, &promo_pos, &promo_key);
 
+			// verifica se houve promoção
+			// se houve, haverá a criação de uma nova raiz
 			if (insert_return == PROMOTION) {
-
+				// cria novo nó raiz
 				INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
+
+				// adiciona a chave/posicao e os filhos no novo nó
 				new_root->nroChavesIndexadas++;
 				new_root->keys[0] = promo_key;
 				new_root->pos[0] = promo_pos;
 				new_root->children[0] = index_header->noRaiz;
 				new_root->children[1] = promo_child;
+
+				// atualiza no header a nova raiz
 				index_header->noRaiz = new_root->RRNdoNo;
 				index_header->RRNproxNo++;
 				
+				// salva o nó em disco
 				escreve_index_data(bin_index, new_root);
 				
 				IS_D printf("NEW ROOT\n");
@@ -398,9 +426,12 @@ ERROR operation10(char *bin_fname, char *bin_index) {
 			}
 		}
 		linha_delete(&linha);
+
+		// salva a posição da próxima linha
 		byteoffset = ftell(bin_data);
 	}
 
+	// salva o header atualizado
 	index_header->status = '1';
 	escreve_header_index(bin_index, index_header);
 	fclose(bin_data);
@@ -423,24 +454,40 @@ ERROR operation11(char *bin_fname, char *bin_index, char *prefixo) {
 	if (index_header==NULL)
 		return FILE_ERROR;
 
+	if (index_header->status == '0') {
+		free(index_header);
+		return FILE_ERROR;
+	}
+	FILE *veiculo_file = fopen(bin_fname, "rb");
+	if (veiculo_file==NULL)
+		return FILE_ERROR;
+
+	VEICULO_HEADER *header_veiculo = bin_get_header_veiculo(veiculo_file);
+	if (header_veiculo->status == '0') {
+		fclose(veiculo_file);
+		free(header_veiculo);
+		free(index_header);
+		return FILE_ERROR;
+	}
+
 	IS_D printf("procurando %d\n", convertePrefixo(prefixo));
+
+	// caso encontre o veiculo, salva sua posição no arquivo de registro
 	int64 veiculo_offset = btree_search(bin_index, index_header->noRaiz, convertePrefixo(prefixo));
 	
+	// se for -1, não foi encontrado veiculo
 	if (veiculo_offset == -1) {
 		free(index_header);
 		return REG_NULL;
 	}
 
-	FILE *veiculo_file = fopen(bin_fname, "rb");
-	if (veiculo_file == NULL) {
-		free(index_header);
-		return FILE_ERROR;
-	}
-
+	// vai para a posição encontrado no arquivo de registro e lê o veiculo
 	fseek(veiculo_file, veiculo_offset, SEEK_SET);
 	VEICULO *veiculo = bin_get_veiculo(veiculo_file, NULL, NULL);
 
 	free(index_header);
+
+	// imprime o veiculo encontrado
 	print_veiculo(veiculo);
 	veiculo_delete(&veiculo);
 	fclose(veiculo_file);
@@ -453,7 +500,21 @@ ERROR operation12(char *bin_fname, char *bin_index, int codLinha) {
 	if (index_header==NULL)
 		return FILE_ERROR;
 
+	FILE *linha_file = fopen(bin_fname, "rb");
+	if (linha_file == NULL)
+		return FILE_ERROR;
+
+	LINHA_HEADER *header_linha = bin_get_header_linha(linha_file);
+	if (header_linha->status == '0') {
+		fclose(linha_file);
+		free(index_header);
+		return FILE_ERROR;
+	}
+
+
 	IS_D printf("procurando %d\n", codLinha);
+
+	// caso encontre o veiculo, salva sua posição no arquivo de registro
 	int64 linha_offset = btree_search(bin_index, index_header->noRaiz, codLinha);
 	
 	if (linha_offset == -1) {
@@ -461,17 +522,20 @@ ERROR operation12(char *bin_fname, char *bin_index, int codLinha) {
 		return REG_NULL;
 	}
 
-	FILE *linha_file = fopen(bin_fname, "rb");
 	if (linha_file == NULL) {
 		free(index_header);
 		return FILE_ERROR;
 	}
 
+	// vai para a posição encontrada no arquivo de registro
 	fseek(linha_file, linha_offset, SEEK_SET);
 	LINHA *linha = bin_get_linha(linha_file, NULL, NULL);
 
 	free(index_header);
+
+	//imprime a linha encontrada
 	print_linha(linha);
+
 	linha_delete(&linha);
 	fclose(linha_file);
 
@@ -481,19 +545,27 @@ ERROR operation12(char *bin_fname, char *bin_index, int codLinha) {
 ERROR operation13(char *bin_fname, char *bin_index, int n) {
 	FILE *veiculo_bin = fopen(bin_fname, "rb");
 
-	INDEX_HEADER *index_header = bin_get_header_index(bin_index);
-	VEICULO_HEADER *header_veiculo = bin_get_header_veiculo(veiculo_bin);
-	fclose(veiculo_bin);
-
-	if (index_header==NULL)
+	if (veiculo_bin==NULL)
 		return FILE_ERROR;
 
-	if (header_veiculo==NULL) {
-		free(index_header);
+	INDEX_HEADER *index_header = bin_get_header_index(bin_index);
+
+	if (index_header==NULL) {
+		fclose(veiculo_bin);
 		return FILE_ERROR;
 	}
 
-	veiculo_bin = fopen(bin_fname, "ab");
+	VEICULO_HEADER *header_veiculo = bin_get_header_veiculo(veiculo_bin);
+
+
+	if (header_veiculo->status=='0' || index_header->status=='0') {
+		free(header_veiculo); free(index_header);
+		return FILE_ERROR;
+	}
+
+	fclose(veiculo_bin);
+
+
 
 	VEICULO *veiculo = NULL;
 	int64 veiculo_offset;
@@ -501,36 +573,54 @@ ERROR operation13(char *bin_fname, char *bin_index, int n) {
 	// parametros para funcão de inserção (não precisam estar com valores atribuidos)
 	int promo_key, promo_child, promo_pos, insert_return=-1;
 
-	for (int i=0; i<n; i++) {	
+	for (int i=0; i<n; i++) {
+		veiculo_bin = fopen(bin_fname, "rb+");
+		// vai para o final do arquivo de registro
+		fseek(veiculo_bin, 0, SEEK_END);
+
+		// atribui a posição final ao byte offset do veiculo a ser inserido na arvore
+		veiculo_offset = ftell(veiculo_bin);
+		header_veiculo->byteProxReg = veiculo_offset;
+
 		veiculo = veiculo_input();
 
-		veiculo_offset = ftell(veiculo_bin);
 		escreve_veiculo(veiculo_bin, header_veiculo, veiculo);
 
 		insert_return = btree_insert(bin_index, index_header, index_header->noRaiz, convertePrefixo(veiculo->prefixo), veiculo_offset, &promo_key, &promo_child, &promo_pos);
 
+		// verifica se houve promoção
+			// se houve, haverá a criação de uma nova raiz
 		if (insert_return == PROMOTION) {
+				// cria novo nó raiz
+			INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
 
-				INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
-				new_root->nroChavesIndexadas++;
-				new_root->keys[0] = promo_key;
-				new_root->pos[0] = promo_pos;
-				new_root->children[0] = index_header->noRaiz;
-				new_root->children[1] = promo_child;
-				index_header->noRaiz = new_root->RRNdoNo;
-				index_header->RRNproxNo++;
+				// adiciona a chave/posicao e os filhos no novo nó
+			new_root->nroChavesIndexadas++;
+			new_root->keys[0] = promo_key;
+			new_root->pos[0] = promo_pos;
+			new_root->children[0] = index_header->noRaiz;
+			new_root->children[1] = promo_child;
+
+				// atualiza no header a nova raiz
+			index_header->noRaiz = new_root->RRNdoNo;
+			index_header->RRNproxNo++;
 				
-				escreve_index_data(bin_index, new_root);
+				// salva o nó em disco
+			escreve_index_data(bin_index, new_root);
 				
-				IS_D printf("NEW ROOT\n");
-				IS_D print_node(new_root);
-				free(new_root);
-			}
+			IS_D printf("NEW ROOT\n");
+			IS_D print_node(new_root);
+			free(new_root);
+		}
 
 		veiculo_delete(&veiculo);
+		fclose(veiculo_bin);
 	}
+	veiculo_bin = fopen(bin_fname, "rb+");
+
+	// escreve o header atualizado em disco
 	escreve_header_index(bin_index, index_header);
-	header_linha_alter_status(veiculo_bin, '1');
+	header_veiculo_alter_status(veiculo_bin, '1');
 	free(header_veiculo);
 	fclose(veiculo_bin);
 	binarioNaTela(bin_index);
@@ -540,22 +630,23 @@ ERROR operation13(char *bin_fname, char *bin_index, int n) {
 
 ERROR operation14(char *bin_fname, char *bin_index, int n) {
 	FILE *linha_bin = fopen(bin_fname, "rb");
+	if (linha_bin==NULL)
+		return FILE_ERROR;
 
 	INDEX_HEADER *index_header = bin_get_header_index(bin_index);
+	if (index_header==NULL) {
+		fclose(linha_bin);
+		return FILE_ERROR;
+	}
 
 	LINHA_HEADER *header_linha = bin_get_header_linha(linha_bin);
 	fclose(linha_bin);
 
-
-	if (index_header==NULL)
-		return FILE_ERROR;
-
-	if (header_linha==NULL) {
-		free(index_header);
+	if (header_linha->status=='0' || index_header->status=='0') {
+		free(header_linha); free(index_header);
 		return FILE_ERROR;
 	}
 
-	linha_bin = fopen(bin_fname, "ab");
 
 	LINHA *linha = NULL;
 	int64 linha_offset;
@@ -564,33 +655,48 @@ ERROR operation14(char *bin_fname, char *bin_index, int n) {
 	int promo_key, promo_child, promo_pos, insert_return=-1;
 
 	for (int i=0; i<n; i++) {	
+		linha_bin = fopen(bin_fname, "rb+");
+
+		// atribui a posição final ao byte offset do veiculo a ser inserido na arvore
+		fseek(linha_bin, 0, SEEK_END);
+		linha_offset = ftell(linha_bin);
+		header_linha->byteProxReg = linha_offset;
+
 		linha = linha_input();
 
-		linha_offset = ftell(linha_bin);
 		escreve_linha(linha_bin, header_linha, linha);
 
 		insert_return = btree_insert(bin_index, index_header, index_header->noRaiz, linha->codLinha, linha_offset, &promo_key, &promo_child, &promo_pos);
 
+		// verifica se houve promoção
+			// se houve, haverá a criação de uma nova raiz
 		if (insert_return == PROMOTION) {
+				// cria novo nó raiz
+			INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
 
-				INDEX_REG *new_root = create_indexreg(index_header->RRNproxNo);
-				new_root->nroChavesIndexadas++;
-				new_root->keys[0] = promo_key;
-				new_root->pos[0] = promo_pos;
-				new_root->children[0] = index_header->noRaiz;
-				new_root->children[1] = promo_child;
-				index_header->noRaiz = new_root->RRNdoNo;
-				index_header->RRNproxNo++;
-				
-				escreve_index_data(bin_index, new_root);
-				
-				IS_D printf("NEW ROOT\n");
-				IS_D print_node(new_root);
-				free(new_root);
-			}
+			// adiciona a chave/posicao e os filhos no novo nó
+			new_root->nroChavesIndexadas++;
+			new_root->keys[0] = promo_key;
+			new_root->pos[0] = promo_pos;
+			new_root->children[0] = index_header->noRaiz;
+			new_root->children[1] = promo_child;
+
+			// atualiza no header a nova raiz
+			index_header->noRaiz = new_root->RRNdoNo;
+			index_header->RRNproxNo++;
+			
+			// salva o nó em disco
+			escreve_index_data(bin_index, new_root);
+			
+			IS_D printf("NEW ROOT\n");
+			IS_D print_node(new_root);
+			free(new_root);
+		}
 
 		linha_delete(&linha);
+		fclose(linha_bin);
 	}
+	linha_bin = fopen(bin_fname, "rb+");
 	escreve_header_index(bin_index, index_header);
 	header_linha_alter_status(linha_bin, '1');
 	free(header_linha);
@@ -599,10 +705,9 @@ ERROR operation14(char *bin_fname, char *bin_index, int n) {
 	return 0;
 }
 
+// funcão para imprimir toda a árvore (para Debug)
 ERROR op22(char *bin_f) {
 	INDEX_HEADER *index_header = bin_get_header_index(bin_f);
 	btree_print(bin_f, 0);
 	free(index_header);
 }
-
-//ERROR operation13(char *bin)
